ea1f7b63154fe4011fd04574b56dee006ed3a83a
OlhaYastrebova <116676978+OlhaYastrebova@users.noreply.github.com>
2024-06-25 19:24:46 +0300

    try-exept

--- task_1.py
+++ task_1.py
@@ -1,14 +1,23 @@
 from datetime import datetime, date
 
 def string_to_date(date_string):
-    return datetime.strptime(date_string, "%Y.%m.%d").date()
+    try:
+        return datetime.strptime(date_string, "%Y.%m.%d").date()
+    except ValueError:
+        print("Incorrect date format, should be YYYY.MM.DD")
+        return None
 
 def get_days_from_today(date_string):
     input_date = string_to_date(date_string)
+    if input_date is None:
+        return "Invalid date format"
+    
     current_date = date.today()
     days = (input_date - current_date).days
     return days
 
-input_date_string = input("Enter a date (YYYY.MM.DD): ")
-days_from_today = get_days_from_today(input_date_string)
+# Введення дати користувачем
+date_string = input("Enter a date (YYYY.MM.DD): ")
+
+days_from_today = get_days_from_today(date_string)
 print(f"Days from today: {days_from_today}")
